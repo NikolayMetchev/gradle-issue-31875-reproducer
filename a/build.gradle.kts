306a7436c8f6806143df9bd2a6be905af467b5e6
Nikolay Metchev <nmetchev@paxos.com>
2025-01-02 14:00:23 +0000

    wip

--- a/build.gradle.kts
+++ a/build.gradle.kts
@@ -1,3 +1,40 @@
 plugins {
     kotlin("jvm") version "2.1.0"
 }
+
+configurations {
+    compileClasspath {
+        resolutionStrategy.activateDependencyLocking()
+    }
+}
+
+dependencyLocking {
+    lockAllConfigurations()
+}
+
+tasks {
+    register("resolveAndLockAll") {
+        notCompatibleWithConfigurationCache("Filters configurations at execution time")
+        doFirst {
+            require(gradle.startParameter.isWriteDependencyLocks)
+        }
+        doLast {
+            configurations
+                .filter {
+                    // Add any custom filtering on the configurations to be resolved
+                    (
+                            it.name !in
+                                    listOf(
+                                        "projectHealthClasspath",
+                                        "resolvedDepsClasspath",
+                                        "testDependencySources",
+                                        "dependencySources",
+                                        "kotlinNativeBundleConfiguration",
+                                        "combinedGraphClasspath",
+                                    )
+                            ) &&
+                            it.isCanBeResolved
+                }.forEach { it.resolve() }
+        }
+    }
+}
--- c/build.gradle.kts
+++ c/build.gradle.kts
@@ -1,3 +1,42 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     kotlin("jvm") version "2.1.0"
+}
+
+configurations {
+    compileClasspath {
+        resolutionStrategy.activateDependencyLocking()
+    }
+}
+
+dependencyLocking {
+    lockAllConfigurations()
+}
+
+tasks {
+    register("resolveAndLockAll") {
+        notCompatibleWithConfigurationCache("Filters configurations at execution time")
+        doFirst {
+            require(gradle.startParameter.isWriteDependencyLocks)
+        }
+        doLast {
+            configurations
+                .filter {
+                    // Add any custom filtering on the configurations to be resolved
+                    (
+                            it.name !in
+                                    listOf(
+                                        "projectHealthClasspath",
+                                        "resolvedDepsClasspath",
+                                        "testDependencySources",
+                                        "dependencySources",
+                                        "kotlinNativeBundleConfiguration",
+                                        "combinedGraphClasspath",
+                                    )
+                            ) &&
+                            it.isCanBeResolved
+                }.forEach { it.resolve() }
+        }
+    }
 }
